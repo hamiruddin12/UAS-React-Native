5804f5564b2127cba4f343ea60109de200cc1613
ahmadsubhann <sbhgaming80@gmail.com>
2024-07-04 13:51:38 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Beranda from './screens/Beranda'
+import Kontak from './screens/Kontak'
+import Profil from './screens/Profil'
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name='Beranda' component={Beranda} />
+        <Stack.Screen name='Kontak' component={Kontak} />
+        <Stack.Screen name='Profil' component={Profil} />
+      </Stack.Navigator>
   )
 }
 
---
+++ src/screens/Kontak.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Kontak = () => {
+  return (
+    <View>
+      <Text>Kontak</Text>
+    </View>
+  )
+}
+
+export default Kontak
+
+const styles = StyleSheet.create({})
